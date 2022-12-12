54725302ebf6f0f7f8b694d569da8d9cdbb00c74
joseg <jalfred.076@gmail.com>
2022-12-12 18:16:56 -0500

    fix

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("y dale (U)!!!")
-<<<<<<< HEAD
 	fmt.Println(("el mejor de los equipos!!!"))
-=======
 	fmt.Println("el más campeón!!!")
->>>>>>> a9e0184... add
 }
