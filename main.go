04500ef6d384e73ed91712d9dfdb1c490774205a
jagch <jalfred.076@gmail.com>
2024-05-08 17:41:25 -0500

    changes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("y dale (U) campeón 2023!!!")
-	functionInRama2()
+	fmt.Println("*******")
+	fmt.Println("y dale (U) campeón 2024!!!")
+	fmt.Println("el apagón, el apagón, el apagón... (bis)")
 }
--- rama1_file.go
+++ rama1_file.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func functionInRama2() {
-	fmt.Println("Iam function in rama2")
+	fmt.Println("I'm fixed in rama3")
+	fmt.Println("=(U)=")
 }
