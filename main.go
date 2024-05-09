4fa07695058614349e3f01798063344447c2997d
jagch <jalfred.076@gmail.com>
2024-05-08 21:04:36 -0500

    changes

--- main.go
+++ main.go
@@ -5,5 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("*******")
 	fmt.Println("y dale (U) campeón 2024!!!")
+	fmt.Println("*******")
 	fmt.Println("el apagón, el apagón, el apagón... (bis)")
+	fmt.Println("*******")
 }
